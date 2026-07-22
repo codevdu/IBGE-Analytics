6ca0315a0bd17a6152aa818b6221072c4eb9df87
Sara <saramagalhaes242@gmail.com>
2026-07-22 18:55:16 -0300

    feat: cria estrutura e logica controlada do dropdown

--- frontend/src/components/dropdown.tsx
+++ frontend/src/components/dropdown.tsx
@@ -1,4 +1,18 @@
-
-export function Dropdown() {
-    // DropDownComponent
+interface DropdownProps{
+    indicador:string;
+    onChangeIndicador: (novoValor: string) => void;
+}
+export default function Dropdown({indicador, onChangeIndicador} : DropdownProps){
+    return(
+        <div className="flex gap-4">
+            <select value = {indicador}
+                onChange = {(evento) => onChangeIndicador(evento.target.value)}>
+                <option value={"populacao"}>populacao</option>
+                <option value={"densidade"}>densidade</option>
+            </select>
+            <select disabled>
+               <option value="">Regiao</option>
+            </select>
+        </div>
+    );
 }
